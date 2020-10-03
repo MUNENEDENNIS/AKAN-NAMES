ae5446098cf902ebdda7b320ea3ee4176fd1bddc
Dennis Munene <officialmunene@gmail.com>
2020-10-03 17:43:07 +0300

    Added "If, Else" to JS

--- js/script.js
+++ js/script.js
@@ -20,3 +20,49 @@ function akan(){
     var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     
     var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa", "Afua","Ama"];
+
+  else {
+    if(birthday===0 && gender==="male") {
+      document.getElementById("display").innerHTML = male [0];
+    }
+    if(birthday===1 && gender==="male") {
+      document.getElementById("display").innerHTML = male [1];
+    }
+    if(birthday===2 && gender==="male") {
+      document.getElementById("display").innerHTML = male [2];
+    }
+    if(birthday===3 && gender==="male") {
+      document.getElementById("display").innerHTML = male [3];
+    }
+    if(birthday===4 && gender==="male") {
+      document.getElementById("display").innerHTML = male [4];
+    }
+    if(birthday===5 && gender==="male") {
+      document.getElementById("display").innerHTML = male [5];
+    }
+    if(birthday===6 && gender==="male") {
+      document.getElementById("display").innerHTML = male [6];
+    }
+    if(birthday===0 && gender==="female") {
+      document.getElementById("display").innerHTML = female [0];
+    }
+    if(birthday===1 && gender==="female") {
+      document.getElementById("display").innerHTML = female [1];
+    } 
+    if(birthday===2 && gender==="female") {
+      document.getElementById("display").innerHTML = female [2];
+    } 
+    if(birthday===3 && gender==="female") {
+      document.getElementById("display").innerHTML = female [3];
+    } 
+    if(birthday===4 && gender==="female") {
+      document.getElementById("display").innerHTML = female [4];
+    } 
+    if(birthday===5 && gender==="female") {
+      document.getElementById("display").innerHTML = female [5];
+    } 
+    if (birthday===6 && gender==="female") {
+      document.getElementById("display").innerHTML = female [6];
+    }  
+  }
+}
