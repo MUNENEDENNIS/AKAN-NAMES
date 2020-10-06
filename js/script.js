518a0e557cc38a362e9cdc335b5d307a9229097d
Dennis Munene <officialmunene@gmail.com>
2020-10-06 14:11:52 +0300

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 function akan() {
-    var year = parseFloat(document.getElementById("year").value);
+    let year = parseFloat(document.getElementById("year").value);
     //console.log(year);
 
-    var month = parseFloat(document.getElementById("month").value);
+    let month = parseFloat(document.getElementById("month").value);
     //console.log(month);
 
-    var day = parseFloat(document.getElementById("day").value);
+    let day = parseFloat(document.getElementById("day").value);
     //console.log(day);
 
-    var century = parseFloat(year / 100) + 1;
+    let century = parseFloat(year / 100) + 1;
     //console.log(century);
 
-    var birthday = Math.round((((century / 4) - 2 * century - 1) + ((5 * (year) / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
+    let birthday = Math.round((((century / 4) - 2 * century - 1) + ((5 * (year) / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
     //console.log(birthday);
 
-    var gender = document.getElementById("gender").value;
+    let gender = document.getElementById("gender").value;
     //console.log(gender);
 
-    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     if (year < 0) {
         alert("Please enter a valid year");
